dc0663bc43e4bc36d4bae0aaa53674b5c439d2a7
Rami Al-Zebak <17zrami@gmail.com>
2019-05-29 15:26:29 +0300

    write files on the downloads folder

--- main.js
+++ main.js
@@ -67,6 +67,6 @@ ipcMain.on('save-data', (event, obj) => {
 
   let filename = uuidv4(); // 
   let json = JSON.stringify(obj);
-  fs.writeFile(`${filename}.json`, json, 'utf8', () => { });
+  fs.writeFile(`~/Downloads/${filename}.json`, json, 'utf8', () => { });
 
 });
